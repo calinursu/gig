be8eaecd92b2103c7c9dbf150085860a130abe8e
calinursu <ursucalin92@gmail.com>
2018-01-21 16:31:42 +0100

    add mobile menu animation

--- src/scripts/Script.js
+++ src/scripts/Script.js
@@ -1,7 +1,9 @@
 import Noise from './components/noise';
 import Scroll from './components/scroll';
+import Menu from './components/menu';
 
 window.addEventListener('load', () => {
   Noise.init();
   Scroll.init();
+  Menu.init();
 });
---
+++ src/scripts/components/menu.js
@@ -0,0 +1,46 @@
+class Menu {
+  constructor(el) {
+    this.el = el; 
+    this.openButton = document.querySelector('.open-mobile-menu');
+    this.closeButton = this.el.querySelector('.close-mobile-menu');
+    this.background = this.el.querySelector('.mobile-menu-container .background');
+
+    this.openButton.addEventListener('click', this.open.bind(this));
+    this.closeButton.addEventListener('click', this.close.bind(this));
+  }
+
+  open() {
+    this.el.classList.remove('is-hidden')
+    this.el.classList.add('is-visible');
+  }
+
+  close() {
+    this.el.classList.remove('is-visible');
+    setTimeout( ()=> this.el.classList.add('is-hidden'), 500 );
+  }
+
+  animationEvent(el) {
+      const transitions = {
+        'animation':'animationend',
+        'OAnimation':'oAnimationEnd',
+        'MozAnimation':'animationend',
+        'WebkitAnimation':'webkitAnimationEnd'
+      }
+
+      for(let t in transitions) {
+        if(el.style[t] !== undefined) {
+          return transitions[t];
+        }
+      }
+    }
+
+  static init(selector = ".mobile-menu-container", base = document) {
+    base.querySelectorAll(selector).forEach(element => {
+      new Menu(element);
+    });
+  }
+}
+
+export default Menu;
+
+

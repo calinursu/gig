9efc9f3bdd0cdd16a2987f444847df5df60ccd0d
calinursu <ursucalin92@gmail.com>
2018-02-03 21:16:52 +0100

    close case when navigating through pages

--- src/scripts/components/nav.js
+++ src/scripts/components/nav.js
@@ -8,6 +8,7 @@ class Nav {
     this.pagesContainer = document.querySelector('.content');
     this.locationOrigin = window.location.origin;
     this.menuClosing = false;
+    this.menu = document.querySelector('.main-elements-container');
 
     // elements for case page, when you have a case open and you want to navigate back to cases
     this.contentContainer = document.querySelector('.case-single-container');
@@ -26,18 +27,33 @@ class Nav {
     this.nextPage = this.pagesContainer.querySelector('.page[data-url="' + this.el.dataset.url + '"]');
     const mobile = document.querySelector('.mobile-menu-container.is-visible');
 
+    this.menu.classList.remove('has-overlay');
+
+    // Close any case when navigating through pages
+    const content = this.contentContainer.querySelector('.case-single.is-visible');
+    if(content !== null) {
+      this.contentContainer.classList.remove('is-visible');
+      content.classList.remove('is-visible'); 
+    }
+ 
+
     if (this.nextPage && !this.nextPage.classList.contains('is-visible')) {
       //history.pushState(null, null, this.locationOrigin + '/' + this.el.dataset.url);
       this.currentPage = this.el.dataset.url;
 
       const that = this;
 
-      mobile.classList.remove('is-visible');
-      setTimeout( () => {
-        mobile.classList.add('is-hidden');
+      if(mobile !== null) {
+         mobile.classList.remove('is-visible');
+        setTimeout( () => {
+          mobile.classList.add('is-hidden');
+          activePage.classList.remove('is-visible');
+          that.changePageDescription();
+        }, 800 );
+      } else {
         activePage.classList.remove('is-visible');
-        that.changePageDescription();
-      }, 800 );
+      }
+     
 
       let animation = animationEvent(activePage);
       activePage.classList.remove('is-visible');
@@ -47,15 +63,15 @@ class Nav {
       this.nextPage.classList.remove('is-hidden');
       window.scrollTo(0,0);
     } else {
-      const content = this.contentContainer.querySelector('.case-single.is-visible');
 
-       mobile.classList.remove('is-visible');
-        setTimeout( () => {
-          mobile.classList.add('is-hidden');
-        }, 800 );
+       if(mobile !== null) {
+         mobile.classList.remove('is-visible');
+          setTimeout( () => {
+            mobile.classList.add('is-hidden');
+          }, 800 );
+       }
 
-      this.contentContainer.classList.remove('is-visible');
-      content.classList.remove('is-visible'); 
+      
       this.section.style.display = 'block';
     }
   }
